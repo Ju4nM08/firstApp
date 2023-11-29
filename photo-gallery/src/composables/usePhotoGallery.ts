5da471ffeb0328b10d0572f2c4abfda8687d3707
Ju4nM08 <152302146+Ju4nM08@users.noreply.github.com>
2023-11-29 04:03:29 +0000

    🚧Build first App: Displaying photos

--- photo-gallery/src/composables/usePhotoGallery.ts
+++ photo-gallery/src/composables/usePhotoGallery.ts
@@ -3,16 +3,30 @@ import { Camera, CameraResultType, CameraSource, Photo } from '@capacitor/camera
 import { Filesystem, Directory } from '@capacitor/filesystem';
 import { Preferences } from '@capacitor/preferences';
 
+const photos = ref<UserPhoto[]>([]);
+
 export const usePhotoGallery = () => {
-    const takePhoto = async () => {
-      const photo = await Camera.getPhoto({
-        resultType: CameraResultType.Uri,
-        source: CameraSource.Camera,
-        quality: 100,
-      });
-    };
-  
-    return {
-      takePhoto,
-    };
+  const takePhoto = async () => {
+    const photo = await Camera.getPhoto({
+      resultType: CameraResultType.Uri,
+      source: CameraSource.Camera,
+      quality: 100,
+    });
+    const fileName = Date.now() + '.jpeg';
+const savedFileImage = {
+  filepath: fileName,
+  webviewPath: photo.webPath,
+};
+
+photos.value = [savedFileImage, ...photos.value];
   };
+
+  return {
+    photos,
+    takePhoto,
+  };
+};
+export interface UserPhoto {
+  filepath: string;
+  webviewPath?: string;
+}
--- photo-gallery/src/views/Tab2Page.vue
+++ photo-gallery/src/views/Tab2Page.vue
@@ -5,12 +5,16 @@
         <ion-title>Photo Gallery</ion-title>
       </ion-toolbar>
     </ion-header>
-    <ion-content :fullscreen="true">
-  <ion-fab vertical="bottom" horizontal="center" slot="fixed">
-    <ion-fab-button @click="takePhoto()">
-      <ion-icon :icon="camera"></ion-icon>
-    </ion-fab-button>
-  </ion-fab>
+    <ion-content>
+  <ion-grid>
+    <ion-row>
+      <ion-col size="6" :key="photo.filepath" v-for="photo in photos">
+        <ion-img :src="photo.webviewPath"></ion-img>
+      </ion-col>
+    </ion-row>
+  </ion-grid>
+
+    <!-- <ion-fab> markup  -->
 </ion-content>
   </ion-page>
 </template>
@@ -31,7 +35,6 @@ import {
   IonToolbar,
 } from '@ionic/vue';
 import { camera, trash, close } from 'ionicons/icons';
-import { usePhotoGallery } from '@/composables/usePhotoGallery';
-
-const { takePhoto } = usePhotoGallery();
+import { usePhotoGallery, UserPhoto } from '@/composables/usePhotoGallery';
+const { photos, takePhoto } = usePhotoGallery();
 </script>
